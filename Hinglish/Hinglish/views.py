232e51ee890836eecdcdb04d393de8c2145efb93
Abdul Rehman Khan <arkspeed@gmail.com>
2017-01-31 17:12:50 +0530

    Done with the Project @5:12

--- Hinglish/Hinglish/views.py
+++ Hinglish/Hinglish/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html')
 def translate(request):
-    return HttpResponse("Hello")
+    original = request.GET['Text1']
+    translation = ''
+    for word in original.split():
+        if word[0] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            translation += word[1:]
+        else:
+            translation += word[0]
+            translation += 'PenCho'
+    return render(request, 'translate.html', {'original':original, 'translate':translation})
+
+
